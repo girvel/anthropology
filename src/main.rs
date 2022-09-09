6ec2e1736d0d7c3917b719add2b8cd3d3afd5616
girvel <widauka@ya.ru>
2022-09-10 02:42:51 +0400

    Save some progress

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,39 @@
-use std::io;
-use tui::{backend::CrosstermBackend, Terminal};
+use std::{io, thread, time::Duration};
+use tui::{
+    backend::CrosstermBackend,
+    widgets::{Widget, Block, Borders},
+    layout::{Layout, Constraint, Direction},
+    Terminal
+};
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
 
 fn main() -> Result<(), io::Error> {
-    let mut terminal = Terminal::new(CrosstermBackend::new(io::stdout()));
+    enable_raw_mode()?;
+    let mut stdout = io::stdout;
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout));
+    
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default()
+            .title("Hello world!")
+            .borders(Borders::ALL);
+        f.render_widget(block, size);
+    })?;
+
+    thread::sleep(Duration::from_millis(1000));
+
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
     Ok(())
 }
