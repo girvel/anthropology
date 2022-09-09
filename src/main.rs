a18f0c664f46a631ec11234418bc9dc5d26d42a9
girvel <widauka@ya.ru>
2022-09-10 01:24:26 +0400

    Setup tui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use tui::{backend::CrosstermBackend, Terminal};
+
+fn main() -> Result<(), io::Error> {
+    let mut terminal = Terminal::new(CrosstermBackend::new(io::stdout()));
+    Ok(())
 }
