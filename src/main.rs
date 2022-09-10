31a7cfec1874fdf0993502d66f6d9b569d0e67fc
girvel <widauka@ya.ru>
2022-09-10 04:52:37 +0400

    Hello TUI!

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
 use std::{io, thread, time::Duration};
 use tui::{
     backend::CrosstermBackend,
-    widgets::{Widget, Block, Borders},
-    layout::{Layout, Constraint, Direction},
+    widgets::{Block, Borders},
     Terminal
 };
 use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    event::{DisableMouseCapture, EnableMouseCapture},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 
 fn main() -> Result<(), io::Error> {
     enable_raw_mode()?;
-    let mut stdout = io::stdout;
+    let mut stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-    let mut terminal = Terminal::new(CrosstermBackend::new(stdout));
-    
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout))?;
+
     terminal.draw(|f| {
         let size = f.size();
         let block = Block::default()
